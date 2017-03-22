5161c4a5aaa20f9532a304d1e78df9ffa3c2abd2
Flamacue <asumner@hawk.iit.edu>
2017-03-21 20:36:43 -0400

    first module

---
+++ hello_world/hello.c
@@ -0,0 +1,19 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Adam K. Sumner");
+MODULE_DESCRIPTION("We all gotta start somewhere");
+
+static int __init hello_init(void){
+  printk(KERN_INFO "I'm now around here! Hello!\n");
+  return 0;
+}
+
+static void __exit hello_cleanup(void){
+  printk(KERN_INFO "Goodbye, but mark my words. I'll be back!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_cleanup);
